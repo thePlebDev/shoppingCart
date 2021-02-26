d2c0f01dd1ee472ab8b9b31738c4b0627032fbb0
Tristan Elliott <>
2021-02-26 16:52:02 -0400

    creating the cart controller

--- src/main/java/com/example/demo/controllers/CartController.java
+++ src/main/java/com/example/demo/controllers/CartController.java
@@ -1,5 +1,36 @@
 package com.example.demo.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.example.demo.models.Cart;
+import com.example.demo.services.CartService;
+
+@ResponseBody
+@RequestMapping("api/v1/cart")
 public class CartController {
+	private final CartService cartService;
+	
+	@Autowired
+	public CartController(CartService cartService) {
+		this.cartService = cartService;
+	}
+	
+	@PostMapping
+	public Cart createCart(@RequestBody Cart cart) {
+		return cartService.createCart(cart);
+	}
+	
+	@GetMapping("{id}")
+	public Cart findCart(@PathVariable Long id) {
+		return cartService.findCart(id);
+	}
+	
+	
 
 }
